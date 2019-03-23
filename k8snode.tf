1ed1df17042495213fcc7dfcb4d18b56ffb02200
Sergey Volchkov <morgul@mail.ru>
2019-03-23 22:35:11 +0300

    Create vm nodes. add count index.

---
+++ k8snode.tf
@@ -0,0 +1,92 @@
+# Basic configuration withour variables
+
+# Define authentification configuration
+provider "vsphere" {
+  # If you use a domain set your login like this "MyDomain\\MyUser"
+  user                 = "${var.user}"
+  password             = "${var.password}"
+  vsphere_server       = "${var.vs_server}"
+  # if you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+#### RETRIEVE DATA INFORMATION ON VCENTER ####
+
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter"
+}
+
+data "vsphere_resource_pool" "pool" {
+  # If you haven't resource pool, put "Resources" after cluster name
+  name          = "Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+data "vsphere_host" "host" {
+  name          = "192.168.88.251"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+# Retrieve datastore information on vsphere
+data "vsphere_datastore" "datastore" {
+  name          = "NnSSDDatastore"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+# Retrieve network information on vsphere
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+# Retrieve template information on vsphere
+data "vsphere_virtual_machine" "template" {
+  name          = "node 100gb"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+#### VM CREATION ####
+
+# Set vm parameters
+resource "vsphere_virtual_machine" "node" {
+  name                 = "node-${count.index}"
+  count                = "${var.vm_count}"
+  num_cpus             = 2
+  memory               = 4096
+  datastore_id         = "${data.vsphere_datastore.datastore.id}"
+  host_system_id       = "${data.vsphere_host.host.id}"
+  resource_pool_id     = "${data.vsphere_resource_pool.pool.id}"
+  guest_id             = "${data.vsphere_virtual_machine.template.guest_id}"
+  scsi_type            = "${data.vsphere_virtual_machine.template.scsi_type}"
+
+  # Set network parameters
+  network_interface {
+    network_id         = "${data.vsphere_network.network.id}"
+  }
+
+  # Use a predefined vmware template has main disk
+  disk {
+    name = "vmdisk-${count.index}.vmdk"
+    size = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+
+    customize {
+      linux_options {
+        host_name = "node-${count.index}"
+        domain    = "test.internal"
+      }
+
+      network_interface {
+        ipv4_address    = "192.168.88.10${count.index}"
+        ipv4_netmask    = 24
+        dns_server_list = ["192.168.88.1"]
+      }
+
+      ipv4_gateway = "192.168.88.1"
+      dns_server_list = ["192.168.88.1"]
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable vm_count {
+  description = "Count of VM"
+  default     = 3
+}
+variable user {
+  description = "Vsphere user"
+}
+variable password {
+}
+variable vs_server {
+  description = "Vsphere server ip addres"
+}
